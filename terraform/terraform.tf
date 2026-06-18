fb6a114aa2a5e4122504cc5865ca92cc70b62ef0
Ela <ela.bala1217@gmail.com>
2026-06-19 00:01:34 +0200

    Fix for app visibility

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -32,14 +32,14 @@ resource "aws_security_group" "app_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["95.160.241.94/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["95.160.241.94/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     description = "Allow all outbound traffic"
